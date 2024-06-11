7b36c4dbe76718c36346431fb23f55d171deb4a6
aravindmathradan <aravindmathradan2@gmail.com>
2024-06-12 00:12:19 +0530

    added feed domain and improved handlers

--- cmd/api/feeds.go
+++ cmd/api/feeds.go
@@ -3,18 +3,50 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/aravindmathradan/semaphore/internal/data"
+	"github.com/aravindmathradan/semaphore/internal/validator"
 )
 
 func (app *application) addFeedHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Add new feed")
+	var input struct {
+		Link string `json:"link"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+
+	if data.ValidateLink(v, input.Link); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	fmt.Fprintf(w, "%v\n", input)
 }
 
 func (app *application) getFeedHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(r)
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
 
-	fmt.Fprintf(w, "Feed ID: %d\n", id)
+	feed := data.Feed{
+		ID:        id,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Title:     "Test feed",
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"feed": feed}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
 }
