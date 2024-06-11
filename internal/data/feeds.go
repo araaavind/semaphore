81c4ed567b7aadf875c755d1990c43b2518a65e8
aravindmathradan <aravindmathradan2@gmail.com>
2024-06-12 00:12:19 +0530

    added feed domain and improved handlers

---
+++ internal/data/feeds.go
@@ -0,0 +1,27 @@
+package data
+
+import (
+	"time"
+
+	"github.com/aravindmathradan/semaphore/internal/validator"
+)
+
+type Feed struct {
+	ID          int64     `json:"id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Link        string    `json:"link"`
+	FeedLink    string    `json:"feed_link,omitempty"`
+	Updated     time.Time `json:"updated,omitempty"`
+	PubDate     time.Time `json:"pub_date,omitempty"`
+	FeedType    string    `json:"feed_type,omitempty"`
+	FeedVersion string    `json:"feed_version,omitempty"`
+	Language    string    `json:"language,omitempty"`
+	Version     int32     `json:"version,omitempty"`
+}
+
+func ValidateLink(v *validator.Validator, link string) {
+	v.Check(validator.NotBlank(link), "link", "link must not be empty")
+}
