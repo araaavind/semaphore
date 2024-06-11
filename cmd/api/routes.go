528c486f0053f3bc6800560ea31ee3857febacd3
aravindmathradan <aravindmathradan2@gmail.com>
2024-06-12 00:10:19 +0530

    added panic recovery

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,9 +9,12 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/feeds", app.addFeedHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/feeds/:id", app.getFeedHandler)
 
-	return router
+	return app.recoverPanic(router)
 }
