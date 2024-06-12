06b9a17d9e6e2540f7744524411ca2a6849f6f2a
aravindmathradan <aravindmathradan2@gmail.com>
2024-06-12 13:17:44 +0530

    openDB and add migrations for users and feeds

---
+++ server/migrations/00001_create_users_table.sql
@@ -0,0 +1,18 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS users (
+	id bigserial PRIMARY KEY,
+    full_name text NOT NULL,
+	username text NOT NULL,
+	email citext UNIQUE NOT NULL,
+	password_hash bytea NOT NULL,
+	version integer NOT NULL DEFAULT 1,
+	created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+	updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
+)
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS users;
+-- +goose StatementEnd
