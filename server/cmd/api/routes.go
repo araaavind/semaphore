ed09fbe8fe2d4ccde3212fc40a9d720f53672b44
aravindmathradan <aravindmathradan2@gmail.com>
2024-06-24 18:59:21 +0530

    server: added getCurrentUser handler

--- server/cmd/api/routes.go
+++ server/cmd/api/routes.go
@@ -15,6 +15,8 @@ Method		Route						Description						Permission			Response
 POST		/users						signup							-					user with 201
 PUT			/users/activate				activate user					-					empty response with 200
 
+GET			/me							get logged in user				-					user with 200
+
 POST		/feeds						add and follow a feed			feeds:write			empty response with 201
 GET			/feeds						list all feeds					-					feed list with 200
 GET			/feeds/:feed_id				get a feed						-					feed with 200
@@ -40,6 +42,8 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/feeds/:feed_id", app.getFeed)
 
 	authenticated := alice.New(app.requireAuthentication)
+
+	router.HandlerFunc(http.MethodGet, "/v1/me", app.getCurrentUser)
 
 	router.Handler(http.MethodPut, "/v1/feeds/:feed_id/followers", authenticated.ThenFunc(app.requirePermission("feeds:follow", app.followFeed)))
 	router.Handler(http.MethodDelete, "/v1/feeds/:feed_id/followers", authenticated.ThenFunc(app.requirePermission("feeds:follow", app.unfollowFeed)))
