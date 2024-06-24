ae6804ee2d2001370e16c61bf22455341f54ff59
aravindmathradan <aravindmathradan2@gmail.com>
2024-06-24 11:47:04 +0530

    server: user activation

--- server/cmd/api/routes.go
+++ server/cmd/api/routes.go
@@ -8,7 +8,7 @@ import (
 
 /*
 POST	/users						signup							user with 201
-
+PUT		/users/activate				activate user					empty response with 200
 
 POST	/feeds						add and follow a feed			empty response with 201
 GET		/feeds						list all feeds					feed list with 200
@@ -26,6 +26,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheck)
 
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUser)
+	router.HandlerFunc(http.MethodPut, "/v1/users/activate", app.activateUser)
 
 	router.HandlerFunc(http.MethodPost, "/v1/feeds", app.addAndFollowFeed)
 	router.HandlerFunc(http.MethodGet, "/v1/feeds", app.listFeeds)
