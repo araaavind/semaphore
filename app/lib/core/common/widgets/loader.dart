945b2932fd154ae9664832be404c214853901228
aravindmathradan <aravindmathradan2@gmail.com>
2024-07-01 23:27:54 +0530

    app: add loader widget

---
+++ app/lib/core/common/widgets/loader.dart
@@ -0,0 +1,23 @@
+import 'package:app/core/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class Loader extends StatelessWidget {
+  final Color? color;
+  final double strokeWidth;
+
+  const Loader({
+    super.key,
+    this.color,
+    this.strokeWidth = 3,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CircularProgressIndicator(
+        strokeWidth: strokeWidth,
+        color: color ?? context.theme.colorScheme.onSurface,
+      ),
+    );
+  }
+}
