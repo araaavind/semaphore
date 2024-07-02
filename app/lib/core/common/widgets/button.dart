5befeedcbd1f4fcd9c985291b03e19be1dc2eaa1
aravindmathradan <aravindmathradan2@gmail.com>
2024-07-03 00:21:35 +0530

    app: signup page ui and common widgets+constants

---
+++ app/lib/core/common/widgets/button.dart
@@ -0,0 +1,67 @@
+import 'package:app/core/common/widgets/widgets.dart';
+import 'package:app/core/constants/ui_constants.dart';
+import 'package:app/core/theme/app_palette.dart';
+import 'package:app/core/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String text;
+  final Color? textColor;
+  final Color? backgroundColor;
+  final Size? fixedSize;
+  final bool isLoading;
+
+  const Button({
+    super.key,
+    required this.text,
+    this.textColor,
+    this.backgroundColor,
+    this.fixedSize,
+    required this.onPressed,
+    this.isLoading = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: backgroundColor ?? context.theme.colorScheme.primary,
+        borderRadius: BorderRadius.circular(UIConstants.buttonRadius),
+      ),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          fixedSize: fixedSize ?? const Size(64, 36),
+          backgroundColor: AppPalette.transparent,
+          shadowColor: AppPalette.transparent,
+        ),
+        onPressed: onPressed,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              text,
+              style: context.theme.textTheme.labelLarge?.copyWith(
+                color: textColor ?? context.theme.colorScheme.onPrimary,
+              ),
+            ),
+            isLoading
+                ? Padding(
+                    padding:
+                        const EdgeInsets.only(left: UIConstants.elementPadding),
+                    child: SizedBox(
+                      height: 14,
+                      width: 14,
+                      child: Loader(
+                        color: context.theme.colorScheme.onPrimary,
+                        strokeWidth: 2,
+                      ),
+                    ),
+                  )
+                : const SizedBox.shrink(),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- app/lib/features/auth/presentation/pages/signup_page.dart
+++ app/lib/features/auth/presentation/pages/signup_page.dart
@@ -1,6 +1,11 @@
+import 'package:app/core/common/widgets/widgets.dart';
+import 'package:app/core/constants/constants.dart';
+import 'package:app/core/theme/theme.dart';
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatefulWidget {
+  static route() => MaterialPageRoute(builder: (context) => const SignupPage());
+
   const SignupPage({super.key});
 
   @override
@@ -8,12 +13,94 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Semaphore',
+      body: Padding(
+        padding: const EdgeInsets.all(UIConstants.pagePadding),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RichText(
+                text: TextSpan(
+                  text: 'Join ',
+                  style: context.theme.textTheme.displaySmall?.copyWith(
+                    fontWeight: FontWeight.w100,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'semaphore',
+                      style: context.theme.textTheme.displaySmall?.copyWith(
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              InputField(
+                hintText: 'Name',
+                controller: nameController,
+              ),
+              const SizedBox(height: 10),
+              InputField(
+                hintText: 'Email',
+                controller: emailController,
+              ),
+              const SizedBox(height: 10),
+              InputField(
+                hintText: 'Password',
+                controller: passwordController,
+                isPassword: true,
+              ),
+              const SizedBox(height: 20),
+              Button(
+                text: 'Sign up',
+                fixedSize: const Size(160, 50),
+                onPressed: () {},
+              ),
+              const SizedBox(height: 20),
+              GestureDetector(
+                onTap: () {
+                  // Navigator.pushAndRemoveUntil(
+                  //   context,
+                  //   LoginPage.route(),
+                  //   (route) => false,
+                  // );
+                },
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Already have an account? ',
+                    style: context.theme.textTheme.bodyMedium,
+                    children: [
+                      TextSpan(
+                        text: 'Log in',
+                        style: context.theme.textTheme.bodyMedium?.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: context.theme.colorScheme.primary,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
