2535c36879f95501e91f21040d517c758eb13a96
aravindmathradan <aravindmathradan2@gmail.com>
2024-07-06 13:40:41 +0530

    sdk: suppress example warning

--- app/packages/smphr_sdk/example/smphr_sdk.dart
+++ app/packages/smphr_sdk/example/smphr_sdk.dart
@@ -2,6 +2,7 @@ import 'package:smphr_sdk/smphr_sdk.dart';
 
 void main() async {
   // WidgetsFlutterBinding.ensureInitialized();
+  // ignore: unused_local_variable
   final semaphore =
       await Semaphore.initialize(baseUrl: 'http://192.168.1.5:5000/v1');
   // use semaphore.client
