dac0c663734cb95a14ad03f0bba5983488de6e7e
aravindmathradan <aravindmathradan2@gmail.com>
2024-07-06 18:13:52 +0530

    sdk: implemented basic session persistence logic

--- app/packages/smphr_sdk/lib/src/auth_client.dart
+++ app/packages/smphr_sdk/lib/src/auth_client.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 
 import 'constants.dart';
@@ -5,14 +7,14 @@ import 'types/auth_exception.dart';
 import 'types/auth_response.dart';
 import 'types/error_response.dart';
 import 'types/semaphore_exception.dart';
-// import 'package:smphr_sdk/src/constants.dart';
-// import 'package:smphr_sdk/src/types/auth_exception.dart';
-// import 'package:smphr_sdk/src/types/auth_response.dart';
-// import 'package:smphr_sdk/src/types/error_response.dart';
-// import 'package:smphr_sdk/src/types/semaphore_exception.dart';
+import 'types/session.dart';
+import 'types/user.dart';
 
 class AuthClient {
   final Dio _dio;
+
+  User? _currentUser;
+  Session? _currentSession;
 
   AuthClient(Dio dio) : _dio = dio;
 
@@ -70,5 +72,37 @@ class AuthClient {
         statusCode: Constants.httpInternalServerErrorCode,
       );
     }
+  }
+
+  /// Set the initial session to the session obtained from local storage
+  Future<void> setInitialSession(String jsonStr) async {
+    final session = Session.fromJson(json.decode(jsonStr));
+    if (session == null) {
+      // sign out to delete the local storage from supabase_flutter
+      UnimplementedError('Implement signout if session does not exist');
+    }
+
+    _saveSession(session!);
+  }
+
+  /// Returns the current logged in user, if any;
+  ///
+  /// Use [currentSession] to determine whether the user has an active session,
+  /// because [currentUser] can be non-null without an active session, such as
+  /// when the user signed up using email and password but has not confirmed
+  /// their email address.
+  User? get currentUser => _currentUser;
+
+  /// Returns the current session, if any;
+  Session? get currentSession => _currentSession;
+
+  void _saveSession(Session session) {
+    _currentSession = session;
+    _currentUser = session.user;
+  }
+
+  void _removeSession() {
+    _currentSession = null;
+    _currentUser = null;
   }
 }
