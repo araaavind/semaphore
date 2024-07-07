275ae1de76003d7fa4dcf46a8eac70f1488f4dc6
aravindmathradan <aravindmathradan2@gmail.com>
2024-07-07 14:56:53 +0530

    sdk: rename sessionLocalStorage to sharedLocalStorage

--- app/packages/smphr_sdk/lib/smphr_sdk.dart
+++ app/packages/smphr_sdk/lib/smphr_sdk.dart
@@ -5,8 +5,10 @@ library semaphore;
 
 export 'src/semaphore.dart';
 export 'src/semaphore_client.dart';
+export 'src/local_storage.dart';
 export 'src/types/semaphore_exception.dart';
 export 'src/types/auth_exception.dart';
 export 'src/types/auth_response.dart';
+export 'src/types/session.dart';
 
 // TODO: Export any libraries intended for clients of this package.
