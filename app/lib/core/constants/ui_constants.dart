2b8c2ba5dbeb2986ab60d845c33b87bb4c7de1cf
aravindmathradan <aravindmathradan2@gmail.com>
2024-07-10 18:00:30 +0530

    app: onboarding login navigation

--- app/lib/core/constants/ui_constants.dart
+++ app/lib/core/constants/ui_constants.dart
@@ -3,4 +3,7 @@ abstract class UIConstants {
   static const buttonBorderRadius = 24.0;
   static const elementPadding = 16.0;
   static const pagePadding = 18.0;
+
+  static const onboardingLoginPageMessage1 = "Your account has been created";
+  static const onboardingLoginPageMessage2 = "Log in to get started...";
 }
--- app/lib/features/auth/presentation/pages/login_page.dart
+++ app/lib/features/auth/presentation/pages/login_page.dart
@@ -10,9 +10,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginPage extends StatefulWidget {
-  static route() => MaterialPageRoute(builder: (context) => const LoginPage());
+  static route({bool isOnboarding = false}) => MaterialPageRoute(
+        builder: (context) => LoginPage(isOnboarding: isOnboarding),
+      );
+  final bool isOnboarding;
 
-  const LoginPage({super.key});
+  const LoginPage({super.key, this.isOnboarding = false});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
@@ -54,76 +57,67 @@ class _LoginPageState extends State<LoginPage> {
             return Form(
               key: formKey,
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  RichText(
-                    text: TextSpan(
-                      text: 'dive in to ',
-                      style: context.theme.textTheme.displaySmall?.copyWith(
-                        fontWeight: FontWeight.w100,
-                        color: context.theme.colorScheme.secondary,
+                  TitleTextSpan(widget: widget),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const SizedBox(height: 20),
+                      AuthField(
+                        hintText: 'Username or Email',
+                        controller: usernameOrEmailController,
                       ),
-                      children: [
-                        TextSpan(
-                          text: 'Semaphore',
-                          style: context.theme.textTheme.displaySmall?.copyWith(
-                            fontWeight: FontWeight.w700,
+                      const SizedBox(height: 10),
+                      AuthField(
+                        hintText: 'Password',
+                        controller: passwordController,
+                        isPassword: true,
+                        autovalidateMode: AutovalidateMode.disabled,
+                      ),
+                      const SizedBox(height: 20),
+                      Button(
+                        text: 'Log in',
+                        fixedSize: const Size(160, 50),
+                        onPressed: () {
+                          if (formKey.currentState!.validate()) {
+                            context.read<AuthBloc>().add(
+                                  AuthLoginEvent(
+                                    usernameOrEmail:
+                                        usernameOrEmailController.text.trim(),
+                                    password: passwordController.text.trim(),
+                                  ),
+                                );
+                          }
+                        },
+                      ),
+                      const SizedBox(height: 20),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            ChooseUsernamePage.route(),
+                          );
+                        },
+                        child: RichText(
+                          text: TextSpan(
+                            text: 'Don\'t have an account? ',
+                            style: context.theme.textTheme.bodyMedium,
+                            children: [
+                              TextSpan(
+                                text: 'Create one',
+                                style: context.theme.textTheme.bodyMedium
+                                    ?.copyWith(
+                                  fontWeight: FontWeight.bold,
+                                  color: context.theme.colorScheme.secondary,
+                                ),
+                              ),
+                            ],
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  AuthField(
-                    hintText: 'Username or Email',
-                    controller: usernameOrEmailController,
-                  ),
-                  const SizedBox(height: 10),
-                  AuthField(
-                    hintText: 'Password',
-                    controller: passwordController,
-                    isPassword: true,
-                    autovalidateMode: AutovalidateMode.disabled,
-                  ),
-                  const SizedBox(height: 20),
-                  Button(
-                    text: 'Log in',
-                    fixedSize: const Size(160, 50),
-                    onPressed: () {
-                      if (formKey.currentState!.validate()) {
-                        context.read<AuthBloc>().add(
-                              AuthLoginEvent(
-                                usernameOrEmail:
-                                    usernameOrEmailController.text.trim(),
-                                password: passwordController.text.trim(),
-                              ),
-                            );
-                      }
-                    },
-                  ),
-                  const SizedBox(height: 20),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        ChooseUsernamePage.route(),
-                      );
-                    },
-                    child: RichText(
-                      text: TextSpan(
-                        text: 'Don\'t have an account? ',
-                        style: context.theme.textTheme.bodyMedium,
-                        children: [
-                          TextSpan(
-                            text: 'Create one',
-                            style: context.theme.textTheme.bodyMedium?.copyWith(
-                              fontWeight: FontWeight.bold,
-                              color: context.theme.colorScheme.secondary,
-                            ),
-                          ),
-                        ],
                       ),
-                    ),
+                    ],
                   ),
                 ],
               ),
@@ -132,5 +126,55 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
     );
+  }
+}
+
+class TitleTextSpan extends StatelessWidget {
+  const TitleTextSpan({
+    super.key,
+    required this.widget,
+  });
+
+  final LoginPage widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return !widget.isOnboarding
+        ? RichText(
+            text: TextSpan(
+              text: 'dive in to ',
+              style: context.theme.textTheme.displaySmall?.copyWith(
+                fontWeight: FontWeight.w100,
+                color: context.theme.colorScheme.secondary,
+              ),
+              children: [
+                TextSpan(
+                  text: 'Semaphore',
+                  style: context.theme.textTheme.displaySmall?.copyWith(
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ],
+            ),
+          )
+        : Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                UIConstants.onboardingLoginPageMessage1,
+                style: context.theme.textTheme.titleLarge?.copyWith(
+                  fontWeight: FontWeight.w100,
+                  color: context.theme.colorScheme.secondary,
+                ),
+              ),
+              Text(
+                UIConstants.onboardingLoginPageMessage2,
+                style: context.theme.textTheme.headlineMedium?.copyWith(
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          );
   }
 }
