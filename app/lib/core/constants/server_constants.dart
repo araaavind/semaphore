f7db9cc59751f0863fa6a36e33100ad9c676da46
aravindmathradan <aravindmathradan2@gmail.com>
2024-07-20 10:27:36 +0530

    app: add missing constant in throttle droppable

--- app/lib/core/constants/server_constants.dart
+++ app/lib/core/constants/server_constants.dart
@@ -1,8 +1,10 @@
 class ServerConstants {
-  static const String baseUrl = 'http://192.168.1.5:5000/v1';
+  static const String baseUrl = 'http://192.168.1.4:5000/v1';
   static String persistSessionKey =
       'sm-${Uri.parse(baseUrl).host.split(".").first}-session';
 
   static const int defaultPaginationPage = 1;
-  static const int defaultPaginationPageSize = 8;
+  static const int defaultPaginationPageSize = 12;
+
+  static const throttleDuration = Duration(milliseconds: 100);
 }
