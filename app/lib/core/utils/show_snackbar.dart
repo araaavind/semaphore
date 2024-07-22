33466d9b3998a10c2d1e4e05203e94b323607bbf
aravindmathradan <aravindmathradan2@gmail.com>
2024-07-22 22:06:54 +0530

    app: ui refresh including snackbar

--- app/lib/core/utils/show_snackbar.dart
+++ app/lib/core/utils/show_snackbar.dart
@@ -1,3 +1,4 @@
+import 'package:app/core/constants/constants.dart';
 import 'package:flutter/material.dart';
 
 void showSnackbar(
@@ -17,6 +18,16 @@ void showSnackbar(
           ),
         ),
         backgroundColor: backgroundColor,
+        behavior: SnackBarBehavior.floating,
+        dismissDirection: DismissDirection.horizontal,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 32.0,
+          vertical: 16.0,
+        ),
+        elevation: 4.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(UIConstants.inputBorderRadius),
+        ),
       ),
     );
 }
