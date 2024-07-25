e161cec597d9dc027da6b31f96cdfcbf1c56b015
aravindmathradan <aravindmathradan2@gmail.com>
2024-07-25 19:26:52 +0530

    app: feed tile and follow button

--- app/lib/features/feed/presentation/widgets/search_paged_list.dart
+++ app/lib/features/feed/presentation/widgets/search_paged_list.dart
@@ -1,11 +1,12 @@
 import 'package:app/core/common/widgets/refresher.dart';
 import 'package:app/core/common/widgets/widgets.dart';
 import 'package:app/core/constants/constants.dart';
-import 'package:app/core/theme/app_theme.dart';
 import 'package:app/features/feed/domain/entities/feed.dart';
 import 'package:flutter/material.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
+
+import 'feed_list_tile.dart';
 
 class SearchPagedList extends StatelessWidget {
   const SearchPagedList({
@@ -26,20 +27,7 @@ class SearchPagedList extends StatelessWidget {
       child: PagedListView<int, Feed>(
         pagingController: _pagingController,
         builderDelegate: PagedChildBuilderDelegate<Feed>(
-          itemBuilder: (context, item, index) => ListTile(
-            title: Text(
-              item.title,
-              style: context.theme.textTheme.bodyLarge!.copyWith(
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            subtitle: Text(
-              item.description ?? '',
-              style: context.theme.textTheme.bodyMedium!.copyWith(
-                fontWeight: FontWeight.w300,
-              ),
-            ),
-          ),
+          itemBuilder: (context, item, index) => FeedListTile(feed: item),
           firstPageErrorIndicatorBuilder: (_) => FirstPageErrorIndicator(
             title: TextConstants.feedListFetchErrorTitle,
             message: _pagingController.error,
@@ -52,10 +40,18 @@ class SearchPagedList extends StatelessWidget {
             message: _pagingController.error,
             onTap: _pagingController.retryLastFailedRequest,
           ),
-          newPageProgressIndicatorBuilder: (_) =>
-              const ShimmerLoader(pageSize: 1),
-          firstPageProgressIndicatorBuilder: (_) => const ShimmerLoader(
-              pageSize: ServerConstants.defaultPaginationPageSize),
+          newPageProgressIndicatorBuilder: (_) => const Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: UIConstants.pagePadding,
+            ),
+            child: ShimmerLoader(pageSize: 2),
+          ),
+          firstPageProgressIndicatorBuilder: (_) => const Padding(
+            padding: EdgeInsets.symmetric(horizontal: UIConstants.pagePadding),
+            child: ShimmerLoader(
+              pageSize: ServerConstants.defaultPaginationPageSize,
+            ),
+          ),
           noMoreItemsIndicatorBuilder: (_) => const NoMoreItemsIndicator(),
           noItemsFoundIndicatorBuilder: (_) => const NoMoreItemsIndicator(),
         ),
