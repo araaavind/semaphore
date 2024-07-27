2db981f51caa809a2eae234f8bcd6547c3e108ed
aravindmathradan <aravindmathradan2@gmail.com>
2024-07-27 10:34:50 +0530

    server: update migrations to add indexes

--- server/migrations/00001_create_users_table.sql
+++ server/migrations/00001_create_users_table.sql
@@ -10,10 +10,14 @@ CREATE TABLE IF NOT EXISTS users (
 	version integer NOT NULL DEFAULT 1,
 	created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
 	updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
-)
+);
+CREATE INDEX IF NOT EXISTS users_id_idx ON user(id);
+CREATE INDEX IF NOT EXISTS users_full_name_idx ON user(full_name);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+DROP INDEX IF EXISTS users_full_name_idx;
+DROP INDEX IF EXISTS users_id_idx;
 DROP TABLE IF EXISTS users;
 -- +goose StatementEnd
--- server/migrations/00004_add_feeds_indexes.sql
+++ server/migrations/00004_add_feeds_indexes.sql
@@ -1,9 +1,11 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE INDEX IF NOT EXISTS feeds_title_idx ON feeds USING GIN (to_tsvector('simple', title));
+CREATE INDEX IF NOT EXISTS feeds_id_idx ON feeds(id);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
+DROP INDEX IF EXISTS feeds_id_idx;
 DROP INDEX IF EXISTS feeds_title_idx;
 -- +goose StatementEnd
