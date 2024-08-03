d61806623ad3e4169a69b1fb2733d7ab7d945f55
aravindmathradan <aravindmathradan2@gmail.com>
2024-08-04 03:31:03 +0530

    app: added remote server baseUrl

--- app/lib/core/constants/server_constants.dart
+++ app/lib/core/constants/server_constants.dart
@@ -1,5 +1,5 @@
 class ServerConstants {
-  static const String baseUrl = 'http://192.168.1.4:5000/v1';
+  static const String baseUrl = 'https://smphr.aravindunnikrishnan.in/v1';
   static String persistSessionKey =
       'sm-${Uri.parse(baseUrl).host.split(".").first}-session';
 
