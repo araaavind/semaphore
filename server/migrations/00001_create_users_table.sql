01dcb86829ceed2d2a643bb71072f7250e68f323
Aravind <aravindmathradan@gmail.com>
2024-08-03 15:08:28 +0530

    server: fix errors in migration files

--- server/migrations/00001_create_users_table.sql
+++ server/migrations/00001_create_users_table.sql
@@ -11,8 +11,8 @@ CREATE TABLE IF NOT EXISTS users (
 	created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
 	updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
-CREATE INDEX IF NOT EXISTS users_id_idx ON user(id);
-CREATE INDEX IF NOT EXISTS users_full_name_idx ON user(full_name);
+CREATE INDEX IF NOT EXISTS users_id_idx ON users(id);
+CREATE INDEX IF NOT EXISTS users_full_name_idx ON users(full_name);
 -- +goose StatementEnd
 
 -- +goose Down
