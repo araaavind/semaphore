399673a27cee7d6fbecfe323541fa747471b1835
aravindmathradan <aravindmathradan2@gmail.com>
2024-08-04 14:19:26 +0530

    server: minor error message update

--- server/cmd/api/errors.go
+++ server/cmd/api/errors.go
@@ -75,12 +75,12 @@ func (app *application) authenticationRequiredResponse(w http.ResponseWriter, r 
 }
 
 func (app *application) inactiveAccountResponse(w http.ResponseWriter, r *http.Request) {
-	message := "Your user account must be activated to access this resource"
+	message := "Your account must be activated to access this resource"
 	app.errorResponse(w, r, http.StatusForbidden, message)
 }
 
 func (app *application) notPermittedResponse(w http.ResponseWriter, r *http.Request) {
-	message := "Your user account doesn't have the necessary permissions to access this resource"
+	message := "Your account doesn't have the necessary permissions to access this resource"
 	app.errorResponse(w, r, http.StatusForbidden, message)
 }
 
