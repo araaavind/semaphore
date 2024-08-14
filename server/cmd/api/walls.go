88e7456e0ee26317843a5a7fe7c29ad4f2171c6c
aravindmathradan <aravindmathradan2@gmail.com>
2024-08-14 12:19:38 +0530

    server: list items for wall

---
+++ server/cmd/api/walls.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/aravindmathradan/semaphore/internal/data"
+	"github.com/aravindmathradan/semaphore/internal/validator"
+)
+
+func (app *application) listItemsForWall(w http.ResponseWriter, r *http.Request) {
+	wallID, err := app.readIDParam(r, "wall_id")
+	if err != nil || wallID < 1 {
+		app.notFoundResponse(w, r)
+		return
+	}
+
+	var input struct {
+		Title string
+		data.Filters
+	}
+
+	v := validator.New()
+	qs := r.URL.Query()
+
+	input.Title = app.readString(qs, "title", "")
+	input.Page = app.readInt(qs, "page", 1, v)
+	input.PageSize = app.readInt(qs, "page_size", 16, v)
+	input.Sort = app.readString(qs, "sort", "-pub_date")
+	input.SortSafeList = []string{"id", "title", "pub_date", "-id", "-title", "-pub_date"}
+
+	if data.ValidateFilters(v, input.Filters); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	wall, err := app.models.Walls.FindByID(wallID)
+	if err != nil {
+		if errors.Is(err, data.ErrRecordNotFound) {
+			app.notFoundResponse(w, r)
+			return
+		}
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	user := app.contextGetSession(r).User
+	if wall.UserID != user.ID {
+		app.notPermittedResponse(w, r)
+		return
+	}
+
+	feeds, err := app.models.WallFeeds.FindFeedsForWall(wallID)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	feedIDs := []int64{}
+	for _, feed := range feeds {
+		feedIDs = append(feedIDs, feed.ID)
+	}
+
+	items, metadata, err := app.models.Items.FindAllForFeeds(feedIDs, input.Title, input.Filters)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"items": items, "metadata": metadata}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
