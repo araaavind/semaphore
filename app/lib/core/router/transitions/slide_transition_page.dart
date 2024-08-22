3d014c998c126c867aeb9ed6b8e0d90cc0510d68
aravindmathradan <aravindmathradan2@gmail.com>
2024-08-22 12:31:47 +0530

    app: updated slidetransition to include direction

--- app/lib/core/router/transitions/slide_transition_page.dart
+++ app/lib/core/router/transitions/slide_transition_page.dart
@@ -1,15 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+enum SlideDirection { leftToRight, rightToLeft, topToBottom, bottomToTop }
+
 class SlideTransitionPage extends CustomTransitionPage<void> {
+  final SlideDirection direction;
+
   SlideTransitionPage({
     required LocalKey super.key,
     required super.child,
+    required this.direction,
   }) : super(
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            Offset beginOffset;
+            switch (direction) {
+              case SlideDirection.leftToRight:
+                beginOffset = const Offset(-1.0, 0.0);
+                break;
+              case SlideDirection.rightToLeft:
+                beginOffset = const Offset(1.0, 0.0);
+                break;
+              case SlideDirection.topToBottom:
+                beginOffset = const Offset(0.0, -1.0);
+                break;
+              case SlideDirection.bottomToTop:
+                beginOffset = const Offset(0.0, 1.0);
+                break;
+            }
+
             return SlideTransition(
               position: animation.drive(Tween(
-                begin: const Offset(1.0, 0.0),
+                begin: beginOffset,
                 end: Offset.zero,
               ).chain(CurveTween(curve: Curves.easeInOut))),
               child: child,
