e4e0e44b73dc973c82496555c5da50895d0123a3
aravindmathradan <aravindmathradan2@gmail.com>
2024-08-22 12:13:36 +0530

    app: webview adblocker and pull to refresh

--- app/lib/features/feed/presentation/pages/web_view.dart
+++ app/lib/features/feed/presentation/pages/web_view.dart
@@ -1,3 +1,5 @@
+import 'package:app/core/theme/app_theme.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
@@ -15,9 +17,89 @@ class WebView extends StatefulWidget {
 }
 
 class _WebViewState extends State<WebView> {
-  final GlobalKey webViewKey = GlobalKey();
-
   InAppWebViewController? webViewController;
+  PullToRefreshController? pullToRefreshController;
+  double progress = 0;
+
+  // list of Ad URL filters to be used to block ads loading.
+  final adUrlFilters = [
+    ".*.doubleclick.net/.*",
+    ".*.ads.pubmatic.com/.*",
+    ".*.googlesyndication.com/.*",
+    ".*.google-analytics.com/.*",
+    ".*.adservice.google.*/.*",
+    ".*.adbrite.com/.*",
+    ".*.exponential.com/.*",
+    ".*.quantserve.com/.*",
+    ".*.scorecardresearch.com/.*",
+    ".*.zedo.com/.*",
+    ".*.adsafeprotected.com/.*",
+    ".*.teads.tv/.*",
+    ".*.outbrain.com/.*"
+  ];
+
+  final List<ContentBlocker> contentBlockers = [];
+
+  @override
+  void initState() {
+    super.initState();
+
+    // for each Ad URL filter, add a Content Blocker to block its loading.
+    for (final adUrlFilter in adUrlFilters) {
+      contentBlockers.add(
+        ContentBlocker(
+          trigger: ContentBlockerTrigger(
+            urlFilter: adUrlFilter,
+          ),
+          action: ContentBlockerAction(
+            type: ContentBlockerActionType.BLOCK,
+          ),
+        ),
+      );
+    }
+
+    // apply the "display: none" style to some HTML elements
+    contentBlockers.add(
+      ContentBlocker(
+        trigger: ContentBlockerTrigger(
+          urlFilter: ".*",
+        ),
+        action: ContentBlockerAction(
+          type: ContentBlockerActionType.CSS_DISPLAY_NONE,
+          selector:
+              ".banner, .banners, .ads, .ad, .advert, .widget-ads, .ad-unit",
+        ),
+      ),
+    );
+
+    pullToRefreshController = kIsWeb
+        ? null
+        : PullToRefreshController(
+            settings: PullToRefreshSettings(
+              distanceToTriggerSync: 100,
+            ),
+            onRefresh: () async {
+              if (defaultTargetPlatform == TargetPlatform.android) {
+                webViewController?.reload();
+              } else if (defaultTargetPlatform == TargetPlatform.iOS) {
+                webViewController?.loadUrl(
+                    urlRequest:
+                        URLRequest(url: await webViewController?.getUrl()));
+              }
+            },
+          );
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (pullToRefreshController != null) {
+      pullToRefreshController!.settings.color =
+          context.theme.colorScheme.onSecondary;
+      pullToRefreshController!.settings.backgroundColor =
+          context.theme.colorScheme.secondary;
+    }
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,17 +117,43 @@ class _WebViewState extends State<WebView> {
           ),
         ],
       ),
-      body: InAppWebView(
-        initialUrlRequest: URLRequest(url: WebUri(widget.url)),
-        onWebViewCreated: (controller) {
-          webViewController = controller;
-        },
-        onLoadStart: (controller, url) {
-          // Handle when loading starts
-        },
-        onLoadStop: (controller, url) {
-          // Handle when loading finishes
-        },
+      body: Stack(
+        children: [
+          InAppWebView(
+            initialUrlRequest: URLRequest(url: WebUri(widget.url)),
+            initialSettings: InAppWebViewSettings(
+              contentBlockers: contentBlockers,
+              algorithmicDarkeningAllowed: true,
+            ),
+            pullToRefreshController: pullToRefreshController,
+            onWebViewCreated: (controller) {
+              webViewController = controller;
+            },
+            onLoadStart: (controller, url) {
+              // Handle when loading starts
+            },
+            onLoadStop: (controller, url) {
+              pullToRefreshController?.endRefreshing();
+            },
+            onReceivedError: (controller, request, error) {
+              pullToRefreshController?.endRefreshing();
+            },
+            onProgressChanged: (controller, progress) {
+              if (progress == 100) {
+                pullToRefreshController?.endRefreshing();
+              }
+              setState(() {
+                this.progress = progress / 100;
+              });
+            },
+          ),
+          progress < 1.0
+              ? LinearProgressIndicator(
+                  value: progress,
+                  color: context.theme.colorScheme.secondary,
+                )
+              : Container(),
+        ],
       ),
     );
   }
