c0136082cffa4702a13eef5ea5f3b952053410a1
aravindmathradan <aravindmathradan2@gmail.com>
2024-08-30 20:46:19 +0530

    app: receive feeds in wall model

--- app/lib/features/feed/domain/entities/wall.dart
+++ app/lib/features/feed/domain/entities/wall.dart
@@ -1,3 +1,4 @@
+import 'package:app/features/feed/domain/entities/feed.dart';
 import 'package:equatable/equatable.dart';
 
 class Wall extends Equatable {
@@ -5,14 +6,16 @@ class Wall extends Equatable {
   final String name;
   final bool isPrimary;
   final int userId;
+  final List<Feed>? feeds;
 
   const Wall({
     required this.id,
     required this.name,
     required this.isPrimary,
     required this.userId,
+    this.feeds,
   });
 
   @override
-  List<Object> get props => [id, name, isPrimary, userId];
+  List<Object?> get props => [id, name, isPrimary, userId, feeds];
 }
