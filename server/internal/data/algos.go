b1346083b4a9ae44bc80f720c10fb696874cc7c0
araaavind <aravindmathradan2@gmail.com>
2025-06-10 13:04:17 +0530

    server: enhance hot score algo with log of like and save count
    - Used Log function on like and save count to prevent linear scaling
    - Increased the smoothing factor from 2.0 to 10.0 to compensate for the log function change. Increased smooth factor reduces over boosting of new items

--- server/internal/data/algos.go
+++ server/internal/data/algos.go
@@ -17,7 +17,7 @@ func buildHotItemsScoreCalculationQuery(likeCountColumn, saveCountColumn, pubDat
 	// Adding a positive smoothing factor will prevent:
 	//   1. Division by 0 for new items (when hours since published is 0)
 	//   2. Over-boosting brand new items with even a single like or save
-	smoothFactor := 2.0
+	smoothFactor := 10.0
 
 	// Gravity exponent is the time based decay factor
 	// It controls the rate at which scores decrease over time
@@ -25,12 +25,16 @@ func buildHotItemsScoreCalculationQuery(likeCountColumn, saveCountColumn, pubDat
 	// Reddit uses 1.8, Hacker News uses 1.5 (Source: chatGPT)
 	gravity := 1.5
 
+	// Formula:
+	// score = (baseScore + log(likeCount + 1) * likeWeight + log(saveCount + 1) * saveWeight) / (timeFactor)
+	// timeFactor = (timeSincePublished + smoothFactor) ^ gravity
+
 	return fmt.Sprintf(`
 		(
 			(
 				%f +
-				COALESCE(%s, 0) * %f +
-				COALESCE(%s, 0) * %f
+				LOG(COALESCE(%s, 0) + 1) * %f +
+				LOG(COALESCE(%s, 0) + 1) * %f
 			)::float /
 			POWER(EXTRACT(EPOCH FROM (now() - COALESCE(%s, %s)))/3600 + %f, %f)
 		)`,
