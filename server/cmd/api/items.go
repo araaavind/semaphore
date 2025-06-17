411adf07b02ffc97c365fdb101971a61bc840dd4
araaavind <aravindmathradan2@gmail.com>
2025-06-17 18:31:14 +0530

    server: cleanup and refactor of background tasks

--- server/cmd/api/items.go
+++ server/cmd/api/items.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/aravindmathradan/semaphore/internal/data"
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/mmcdole/gofeed"
@@ -85,4 +87,28 @@ func CopyItemsFields(parsedFeed *gofeed.Feed, feedID int64) (items []*data.Item)
 		items = append(items, item)
 	}
 	return items
+}
+
+func (app *application) CleanupOldUnsavedItems() {
+	for {
+		select {
+		case <-app.ctx.Done():
+			app.logger.Info("items cleanup shutting down gracefully")
+			return
+		default:
+			startTime := time.Now()
+			err := app.models.Items.CleanupItems(startTime.Add(-1 * app.config.cleanup.itemsCleanupBeforeDuration))
+			if err != nil {
+				app.logInternalError("app.models.Items.CleanupItems failed", err)
+			}
+			timer := time.NewTimer(time.Until(startTime.Add(app.config.cleanup.itemsCleanupPeriod)))
+			select {
+			case <-app.ctx.Done():
+				timer.Stop()
+				return
+			case <-timer.C:
+				// Continue with the next iteration
+			}
+		}
+	}
 }
--- server/cmd/api/tokens.go
+++ server/cmd/api/tokens.go
@@ -521,3 +521,26 @@ func (app *application) createGoogleAuthenticationToken(w http.ResponseWriter, r
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *application) CleanupTokens() {
+	for {
+		select {
+		case <-app.ctx.Done():
+			return
+		default:
+			startTime := time.Now()
+			err := app.models.Tokens.DeleteExpiredTokens()
+			if err != nil {
+				app.logInternalError("app.models.Tokens.CleanupTokens failed", err)
+			}
+			timer := time.NewTimer(time.Until(startTime.Add(app.config.cleanup.tokensCleanupPeriod)))
+			select {
+			case <-app.ctx.Done():
+				timer.Stop()
+				return
+			case <-timer.C:
+				// Continue with the next iteration
+			}
+		}
+	}
+}
